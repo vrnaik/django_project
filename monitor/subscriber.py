64f708ffa576e350fa1bcbddf3fc64f25b0de23d
Vikas Naik <vikasnaikpro@gmail.com>
2019-05-01 20:12:26 +0530

    successfully deployed on nginx web server

--- monitor/subscriber.py
+++ monitor/subscriber.py
@@ -7,39 +7,68 @@ import paho.mqtt.client as mqtt
 
 def on_connect(client, userdata, flags, rc):
     client.subscribe("topic/test")
-       
+   
    
 def on_message(client, userdata, msg):
-    # if msg.payload.decode() == "Hello world!":
-    if msg.topic == "topic/test":
-        print(msg.topic)
-        print(msg.payload.decode())
-        print("its test topic")
+    if msg.topic == "topic/test":        
+        print("\nreceived: "+msg.payload.decode())
         client.disconnect()
 
 def pub():
     client = mqtt.Client()
-    ms = input('type message')
-    client.connect("www.viresor.com",1883,60)
+    ms = input('\ntype message: ')
+    client.connect(broker,port,60)
     client.publish("topic/test", str(ms));
     client.disconnect();
+    wait()
+
+
+def rep():
+    client = mqtt.Client()
+    client.connect(broker,port,60)
+    client.publish("topic/test", 'hmm');
+    client.disconnect();
+    
+def out():
+    client = mqtt.Client()
+    client.connect(broker,port,60)
+    client.publish("topic/test", 'bye');
+    client.disconnect();    
+    
+def wait():
+    print('\nwaiting for reply')   
+    client.loop_forever()       
 
 
 #     www.viresor.com
 if __name__ == "__main__":
+    broker="localhost"
+    port=1883
 
-    client = mqtt.Client("www.viresor.com", 1883, 60)
+    client = mqtt.Client()
+
     while True:
-        client.connect("www.viresor.com", 1883, 60)
+        client.connect(broker, port, 60)
         client.on_connect = on_connect
         client.on_message = on_message
-        print('want to send message')
-        a = input('want to send message type y otherwise n')
-        if a == 'y' :
-            pub()
-        else:
-            client.publish("topic/test", 'nothing to say');
+        
             
+            
+        a = input('\n y[send message ], n[nothing to say], o[over and out], s[start listening]: ')
+        if a == 'y' :
+            pub() 
+        
+        if a == 'n' :
+            rep()
+            
+        if a == 'o' :
+            out()    
+            
+        if a == 's' :
+            print('\nserver is listening')    
+            client.loop_forever()     
+            
+#         print('\nwaiting for reply')    
         client.loop_forever()
 
 
